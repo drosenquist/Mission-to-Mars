03c4b03ad1036f9ad224127b14b22e659c9e9a03
drosenquist <101379969+drosenquist@users.noreply.github.com>
2022-06-04 17:25:50 -0700

    Adding and updating challange files.

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
+
+# Set up Flask
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+# Set up app routes
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
@@ -19,6 +22,5 @@ def scrape():
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
-
 if __name__ == "__main__":
-    app.run()
+   app.run(debug=True)
